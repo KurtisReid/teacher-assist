dcf713443376ee9c61cab16562bedd0efc3a65eb
unknown <kreid8@RCL200-09.kent.edu>
2015-07-01 16:58:55 -0400

    added and linked up classes

---
+++ Chapter_material.h
@@ -0,0 +1,35 @@
+// chapter_material.h
+/*
+	most chapter classes will derive from this class
+
+
+*/
+
+#include<string>
+#include<stack>
+using namespace std;
+
+class Chapter_material
+{
+	public:
+		Chapter_material();
+		Chapter_material();
+		
+	private:
+		string chapter_name;
+		int chapter_num;
+		int chapter_sections;
+		
+	
+	
+};
+
+class Chapter_subsections: public Chapter_material
+{
+	public:
+		void set_information(string str);
+		void select_HW_material(stack<string>& HW_problems_stack);// user selects what material was covered and calls get problems
+		void get_problems(stack<string>& problems_stack);//stack passed by reference
+		
+	
+}
--- class_material.h
+++ class_material.h
@@ -4,16 +4,22 @@
 
 
 */
-
+#include "Chapter_material.h"
 #include<string>
+#include<map>
+#include<stack>
 using namespace std;
 
 class Class_material
 {
 	public:
-	
-	
+		Class_material();
+		Class_material(map<int, Chapter_material>, stack<string>);
+		void create_ws(stack<string>& HW_problems_stack, map<int, Chapter_material> book);
+		
 	private:
+		map<int, Chapter_material> book;//<int is chapter number, 
+		stack<string> HW_problems_stack;// clears at every day
 	
 	
 	
