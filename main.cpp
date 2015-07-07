6b04a80e42706de7f6958ff61ce4aff1a8328ebe
unknown <kreid8@RCL100-17.kent.edu>
2015-07-07 12:42:46 -0400

    added main

--- main.cpp
+++ main.cpp
@@ -7,3 +7,17 @@
 
 
 */
+#include "Chapter_subsections.h"
+#include "class_info.h"
+#include "class_material.h"
+#include "Chapter_material.h"
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	
+	
+	
+}
