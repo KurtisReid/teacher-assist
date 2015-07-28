c8b412af924102504d8a34db2fe8cff2b1221810
Kurtis Reid <kreid8@kent.edu>
2015-07-28 14:14:26 -0400

    created chapter_material.cpp

---
+++ Chapter_material.cpp
@@ -0,0 +1,25 @@
+//Chapter_material.cpp
+
+
+#include "Chapter_subsections.h"
+#include "Chapter_material.h"
+#include<string>
+#include<stack>
+#include<map>
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+Chapter_material::Chapter_material()
+{
+	
+}
+Chapter_material::Chapter_material(string ch_name, int ch_num, int ch_sects, map<int, Chapter_subsections> sect_map)
+{
+	
+}
+void Chapter_material::pick_sections(ofstream& writefile)
+{
+	
+}
